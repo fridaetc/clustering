81eeddf12fb7bc0894b90fcb406e3294eeccefa0
Frida Kallberg <Frida Kallberg>
2018-11-25 20:31:24 +0000

    fix obj format

--- app/src/api.js
+++ app/src/api.js
@@ -9,7 +9,7 @@ const getHeaders = function(token) {
 const get = function(route, token, cb) {
   axios.get(baseUrl + route, getHeaders(token)).then((res) => {
     if(res.status === 200 && res.data.data) {
-      cb(res.data, null);
+      cb(res.data.data, null);
     } else {
       cb(null, res.data.message);
     }
